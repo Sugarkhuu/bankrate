0cc4d1556a602658511690eb1b912dedc9d71049
Sugarkhuu <sugarkhuul@gmail.com>
2018-10-07 13:37:26 +0200

    config.py

--- config.py
+++ config.py
@@ -5,12 +5,8 @@ Created on Tue Oct  2 22:49:20 2018
 @author: Sugar2
 """
 
-mail_list=["sugarkhuul@gmail.com","g.temuulentemka@gmail.com"]
+mail_list=["",""]
 
-From = "sugarkhuul@gmail.com"
+From = ""
 PASSWORD = ""
 To='I will not tell you'
-# To=", ".join(mail_list)
-#To="17034152@brookes.ac.uk"
-#To="gombo.kh@xacbank.mn"
-#To ="Sdoyunerdene@gmail.com"
