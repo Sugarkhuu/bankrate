57cf4baccf6c202fdae88959f1bb27a142831659
sugarkhuu <sugarkhuu.radnaa@ogresearch.com>
2020-11-16 21:04:47 +0800

    removing recipient info

--- config.py
+++ config.py
@@ -7,10 +7,10 @@ Created on Tue Oct  2 22:49:20 2018
 
 import getpass
 
-mail_list=["sugarkhuul@gmail.com"] # list of emails to send the info to
+mail_list=[""] # list of emails to send the info to
 
-From = "sugarkhuul@gmail.com" # the email address to send the info from
-PASSWORD = "Y+;Z>d9MtF" 
+From = "" # the email address to send the info from
+PASSWORD = "" 
 #getpass.getpass('Enter your email password, please:')
 To=''
 ##
