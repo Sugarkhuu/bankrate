96105843a639357ab77f0bfce85dda16f0b893a0
sugarkhuu <sugarkhuu.radnaa@ogresearch.com>
2020-11-16 20:54:12 +0800

    adding recipient info

--- config.py
+++ config.py
@@ -7,10 +7,10 @@ Created on Tue Oct  2 22:49:20 2018
 
 import getpass
 
-mail_list=[""] # list of emails to send the info to
+mail_list=["sugarkhuul@gmail.com"] # list of emails to send the info to
 
-From = "" # the email address to send the info from
-PASSWORD = "" 
+From = "sugarkhuul@gmail.com" # the email address to send the info from
+PASSWORD = "Y+;Z>d9MtF" 
 #getpass.getpass('Enter your email password, please:')
 To=''
 ##
